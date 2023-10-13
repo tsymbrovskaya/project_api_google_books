3b57260e0b7d74ea924302f1142a2f8d9ee2de28
Ксения Цымбровская <kseniacymbrovskaa@MacBook-Air-Ksenia.local>
2023-10-13 20:52:50 +0300

    удалила папку node_modules и output

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import {main as catalogMain, loadMore, addToCart, fetchCategoryBooks} from './js/catalog.js'
 import {main as sliderMain, displaySlide} from "./js/slider.js";
-import ('./css/style.css')
 
 
 window.loadMore = loadMore
